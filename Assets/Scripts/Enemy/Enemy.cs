6f1d03fea521f6223ba9f6e8e8b6a76ea478aa8f
LukeRS2 <88062391+LukeRS2@users.noreply.github.com>
2023-08-10 10:53:44 +1200

    gave the enemy the ability to detect the player

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,18 +12,53 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private string currentState;
     public Path path;
-    // Start is called before the first frame update
+    private GameObject player;
+    public float sightDistance = 20f;
+    public float fieldOfView = 85f;
+    public float eyeHeight;
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CanSeePlayer();
         animator.Play("Run Forward In Place");
+    }
+    public bool CanSeePlayer()
+    {
+        if (player != null)
+           
+        {
+            //is the player close enough to be seen?
+            if (Vector3.Distance(transform.position, player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if (angleToPlayer >= -fieldOfView && angleToPlayer < fieldOfView)
+                {
+                    Ray ray = new Ray(transform.position + (Vector3.up * eyeHeight), targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+                    if (Physics.Raycast(ray, out hitInfo, sightDistance))
+                    {
+
+                        Debug.Log(hitInfo);
+                        if (hitInfo.transform.gameObject == player)
+                        {
+                            Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+                            return true;
+                        }
+                    }
+                    
+                }
+            }
+        }
+        return false;
     }
 }
