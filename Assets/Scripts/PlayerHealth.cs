dd50d8cdd6b499cc63bb2004818e80868890a1af
LukeRS2 <88062391+LukeRS2@users.noreply.github.com>
2023-08-17 10:00:14 +1200

    minor changes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -33,7 +33,6 @@ public class PlayerHealth : MonoBehaviour
     }
     public void UpdateHealthUI()
     {
-        Debug.Log(health);
         float fillF = frontHealthBar.fillAmount;
         float fillB = backHealthBar.fillAmount;
         float hFraction = health / maxHealth;
